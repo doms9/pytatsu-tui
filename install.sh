00000d959bca54d4f77f3e6cd2ae25b0042f7b18
doms9 <96013514+doms9@users.noreply.github.com>
2023-01-27 12:36:12 -0500

    edit install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+failed() {
+    printf '%s\n' "$1" >&2
+    exit "${2-1}"
+}
+
 [ -d "dist" ] && rm -rf dist/*
 
-[ -x "$(command -v python3)" ] || echo "python3 is not installed" && exit 1
+[ -x "$(command -v python3)" ] || failed "python3 is not installed"
 
-[ -x "$(command -v poetry)" ] && poetry build || echo "poetry is not installed" && exit 1
+[ -x "$(command -v poetry)" ] && poetry build || failed "poetry is not installed"
 
 python3 -m pip install $(ls dist/*.tar.gz)
